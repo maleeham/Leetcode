d31a04b91c5da5c8b99a5183a8bf6c91564ebc5c
Maleeha S. Koul <42660845+msk180001@users.noreply.github.com>
2018-12-13 11:42:28 -0600

    Update Range Addition II.java

--- Range Addition II.java
+++ Range Addition II.java
@@ -9,3 +9,10 @@ class Solution {
         
     }
 }
+
+
+/*Given an m * n matrix M initialized with all 0's and several update operations.
+
+Operations are represented by a 2D array, and each operation is represented by an array with two positive integers a and b, which means M[i][j] should be added by one for all 0 <= i < a and 0 <= j < b.
+
+You need to count and return the number of maximum integers in the matrix after performing all the operations.*/
