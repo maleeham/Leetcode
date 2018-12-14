30c1eb26853cb6edf9ad1b35e778830e8b523c09
Maleeha S. Koul <42660845+msk180001@users.noreply.github.com>
2018-12-14 11:48:39 -0600

    Create LargestNumberAtleastTwice.java

---
+++ LargestNumberAtleastTwice.java
@@ -0,0 +1,21 @@
+/*In a given integer array nums, there is always exactly one largest element.
+
+Find whether the largest element in the array is at least twice as much as every other number in the array.
+
+If it is, return the index of the largest element, otherwise return -1.*/
+
+
+class Solution {
+    public int dominantIndex(int[] nums) {
+        int maxIndex = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] > nums[maxIndex])
+                maxIndex = i;
+        }
+        for (int i = 0; i < nums.length; ++i) {
+            if (maxIndex != i && nums[maxIndex] < 2 * nums[i])
+                return -1;
+        }
+        return maxIndex;
+    }
+}
